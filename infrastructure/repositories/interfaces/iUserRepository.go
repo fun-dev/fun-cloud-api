ef63122d9b2f8f3493990746f201c7f347663bd6
tozastation <b1016031@fun.ac.jp>
2018-11-12 16:46:32 +0900

    #4 add FindByToken Func

--- infrastructure/repositories/interfaces/iUserRepository.go
+++ infrastructure/repositories/interfaces/iUserRepository.go
@@ -2,9 +2,11 @@ package interfaces
 
 import "github.com/fun-dev/cloud-api/domain/models"
 
+// IUserRepository is userRepository of interface
 type IUserRepository interface {
 	Insert(*models.User) error
 	FindById(int64) (*models.User, error)
+	FingByToken(string) (*models.User, error)
 	Update(*models.User) error
 	Delete(int64) error
 }
