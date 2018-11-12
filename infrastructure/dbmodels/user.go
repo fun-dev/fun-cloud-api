21aa92392ac1d738691263fffaf22880f2ebcda1
tozastation <b1016031@fun.ac.jp>
2018-11-12 16:39:17 +0900

    #4 add dbmodel

--- infrastructure/dbmodels/user.go
+++ infrastructure/dbmodels/user.go
@@ -2,10 +2,12 @@ package dbmodels
 
 import "time"
 
+// User is syncronizing database model
 type User struct {
-	Id      int64
-	Name    string
-	Age     int
-	Created time.Time `xorm:"created"`
-	Updated time.Time `xorm:"updated"`
+	ID          int64
+	IconURL     string
+	GoogleName  string
+	AccessToken string
+	Created     time.Time `xorm:"created"`
+	Updated     time.Time `xorm:"updated"`
 }
