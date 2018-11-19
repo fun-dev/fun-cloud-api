0bf0d8761406c5849e8edc731fe64f41ce01d833
youtangai <youtanagai@gmail.com>
2018-11-19 15:33:24 +0900

    add container viewmodel

---
+++ application/viewmodels/container.go
@@ -0,0 +1,8 @@
+package viewmodels
+
+type Container struct {
+	ID          int    `json:id`
+	ImageID     int    `json:image_id`
+	ConnectInfo string `json:connect_info`
+	Status      string `json:status`
+}
