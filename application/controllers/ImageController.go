23d3e444dfbd5c53ed9774a9761283c9ba824a55
Shiranui Nui <shiranuisayaka@gmail.com>
2018-12-10 14:31:22 +0900

    #5 [Add] Get Images Description from Docker-Registry

--- application/controllers/ImageController.go
+++ application/controllers/ImageController.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
-	"io/ioutil"
+	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 
+	"github.com/fun-dev/cloud-api/application/viewmodels"
 	"github.com/gin-gonic/gin"
+	"github.com/heroku/docker-registry-client/registry"
 )
 
 type ImageController struct {
@@ -13,20 +16,51 @@ type ImageController struct {
 
 func (ctrl ImageController) Get(c *gin.Context) {
 	// TODO: Remove Hard-Cord URL
-	req, _ := http.NewRequest("GET", "https://registry.cloud.fun.aigis.pw/v2/_catalog", nil)
-	req.Header.Add("authorization", "Basic ZnVuLWljdC1jbG91ZDpCaVdzUFNtbzdWNnY0SQ==")
-	client := new(http.Client)
-	res, err := client.Do(req)
+	url := "https://registry.cloud.fun.aigis.pw/"
+	username := "fun-ict-cloud"
+	password := "BiWsPSmo7V6v4I"
+	hub, err := registry.New(url, username, password)
 	if err != nil {
 		log.Fatal(err)
+		return
+	}
+	repositories, err := hub.Repositories()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	imageData := []viewmodels.Image{}
+	for _, repositry := range repositories {
+		manifest, err := hub.Manifest(repositry, "latest")
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		repositoryName := manifest.Name
+		for _, history := range manifest.History {
+			v1CompatibilityJson := strings.Replace(history.V1Compatibility, "\n", "", -1)
+			var v1CompatibilityObject v1Compatibility
+			err := json.Unmarshal([]byte(v1CompatibilityJson), &v1CompatibilityObject)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			repositryDescription := v1CompatibilityObject.Container_Config.Labels.Description
+			if repositryDescription != "" {
+				repositoryImageData := viewmodels.Image{Name: repositoryName, Description: repositryDescription}
+				imageData = append(imageData, repositoryImageData)
+				break
+			}
+		}
 	}
 
-	// body
-	defer res.Body.Close()
-	body, error := ioutil.ReadAll(res.Body)
-	if error != nil {
-		log.Fatal(error)
-	}
+	c.JSON(http.StatusOK, imageData)
+}
 
-	c.JSON(http.StatusOK, string(body))
+type v1Compatibility struct {
+	Container_Config struct {
+		Labels struct {
+			Description string `json:description`
+		} `json:Labels`
+	} `json:"container_config"`
 }
