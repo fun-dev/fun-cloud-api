b6348929a89e3a222d61b52169d5d7266e01fc34
youtangai <youtanagai@gmail.com>
2019-01-11 16:20:24 +0900

    main.go内のエンドポイント修正 & コメントの付与とコードの見やすさ向上

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/fun-dev/cloud-api/application"
 	"github.com/fun-dev/cloud-api/config"
-	_ "github.com/fun-dev/cloud-api/infrastructure"
 	"github.com/fun-dev/cloud-api/infrastructure/dbmodels"
 	"github.com/fun-dev/cloud-api/middleware"
 	"github.com/gin-gonic/gin"
@@ -23,26 +22,63 @@ func main() {
 }
 
 func setupRouter() *gin.Engine {
+	// routerの生成
 	router := gin.Default()
+
+	// healthチェック用のエンドポイント作成
 	router.GET("/health", func(c *gin.Context) {
 		c.Status(http.StatusOK)
 	})
 
+	// 各種コントローラのインスタンス取得
 	usrCtrl := application.UserController
-
-	router.Use(middleware.TokenAuthMiddleware())
-	router.GET("/users", usrCtrl.Get)
-	router.POST("/users", usrCtrl.Create)
-	router.PUT("/users", usrCtrl.Update)
 	imgCtrl := application.ImageController
-	router.GET("/images", imgCtrl.Get)
-	router.GET("/containers", application.ContainerController.Get)
-	router.POST("/containers/user/:id", application.ContainerController.Post)
+	containerCtrl := application.ContainerController
+
+	// JWTTokenを確認するmiddlewareの登録
+	router.Use(middleware.TokenAuthMiddleware())
+
+	// routerグループの作成
+	v1 := router.Group("/api/v1")
+
+	// users api
+	v1.GET("/users", usrCtrl.Get)
+	v1.POST("/users", usrCtrl.Create)
+	v1.PUT("/users", usrCtrl.Update)
+
+	// images api
+	v1.GET("/images", imgCtrl.Get)
+
+	// containers api
+	v1.GET("/containers", containerCtrl.Get)
+	v1.POST("/containers", containerCtrl.Post)
+	v1.DELETE("/containers/:id", containerCtrl.Delete)
+
 	return router
 }
 
+// 初回起動時に呼ぶ関数
+// 構造体からテーブル定義を自動生成
 func migrate() {
-	connectionString := fmt.Sprintf(
+	connectionString := getConnectionString()
+
+	// DB接続
+	engine, err := xorm.NewEngine("mysql", connectionString)
+	if err != nil {
+		panic(err)
+	}
+	// 接続を閉じるようにしておく
+	defer engine.Close()
+
+	// 構造体からテーブル生成
+	if err := engine.Sync2(new(dbmodels.User)); err != nil {
+		panic(err)
+	}
+}
+
+// 接続文字列を取得する関数
+func getConnectionString() string {
+	return fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True",
 		config.GetSQLUser(),
 		config.GetSQLPass(),
@@ -50,13 +86,4 @@ func migrate() {
 		config.GetSQLPort(),
 		config.GetSQLDB(),
 	)
-	engine, err := xorm.NewEngine("mysql", connectionString)
-	if err != nil {
-		panic(err)
-	}
-	defer engine.Close()
-
-	if err := engine.Sync2(new(dbmodels.User)); err != nil {
-		panic(err)
-	}
 }
