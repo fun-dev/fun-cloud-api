85f5dd32396f388cef2ec80333cd9f63e68e1fed
youtangai <youtanagai@gmail.com>
2019-01-12 19:47:43 +0900

    永井のgoogle unique idでgke上にpodを作成するcontainerRepositoryのテストを記述しました．

--- infrastructure/repositories/containerRepository_test.go
+++ infrastructure/repositories/containerRepository_test.go
@@ -13,3 +13,11 @@ func Test_containerRepository_GetContainersByNamespace(t *testing.T) {
 	}
 	fmt.Printf("%+v\n", containers)
 }
+
+func Test_containerRepository_CreateContainer(t *testing.T) {
+	repo := NewContainerRepository()
+	err := repo.CreateContainer("108898913206828981410", "nginx:latest")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
