6d5da6af6f305035178f1bb3e6cfcf46b9ceee82
youtangai <youtanagai@gmail.com>
2019-01-12 13:20:11 +0900

    環境変数からkubeconfigのパスを取得するconfigの追加およびconfigのテスト追加

--- config/config.go
+++ config/config.go
@@ -3,11 +3,12 @@ package config
 import "github.com/kelseyhightower/envconfig"
 
 type Config struct {
-	SQLUser string `envconfig:"SQL_USER" default:"root"`
-	SQLPass string `envconfig:"SQL_PASS" default:"zJ6pF57r54JH"`
-	SQLHost string `envconfig:"SQL_HOST" default:"127.0.0.1"`
-	SQLPort string `envconfig:"SQL_PORT" default:"3306"`
-	SQLDB   string `envconfig:"SQL_DB" default:"cloud-api"`
+	SQLUser        string `envconfig:"SQL_USER" default:"root"`
+	SQLPass        string `envconfig:"SQL_PASS" default:"zJ6pF57r54JH"`
+	SQLHost        string `envconfig:"SQL_HOST" default:"127.0.0.1"`
+	SQLPort        string `envconfig:"SQL_PORT" default:"3306"`
+	SQLDB          string `envconfig:"SQL_DB" default:"cloud-api"`
+	KubeConfigPath string `envconfig:"K8S_CONFIG_PATH"`
 }
 
 const (
@@ -38,4 +39,8 @@ func GetSQLPort() string {
 
 func GetSQLDB() string {
 	return c.SQLDB
+}
+
+func GetKubeConfigPath() string {
+	return c.KubeConfigPath
 }
