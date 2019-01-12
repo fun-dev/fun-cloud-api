265e0f0f2c87c965b8b9805f137999e923289931
youtangai <youtanagai@gmail.com>
2019-01-12 20:20:27 +0900

    コンテナを作成するContainerControllerのPostを検証するtestを記述

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -8,12 +9,14 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/fun-dev/cloud-api/application/viewmodels"
+
 	"github.com/fun-dev/cloud-api/domain/models"
 	"github.com/stretchr/testify/assert"
 )
 
 const (
-	testJWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhhYWQ2NmJkZWZjMWI0M2Q4ZGIyN2U2NWUyZTJlZjMwMTg3OWQzZTgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDMxMjk0OTY4MjgtcGh1OGQzOXIyZ2V0YWQ4OHEzbWZ0aWZpdTkwcjYzNG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MzEyOTQ5NjgyOC1waHU4ZDM5cjJnZXRhZDg4cTNtZnRpZml1OTByNjM0bi5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwODg5ODkxMzIwNjgyODk4MTQxMCIsImVtYWlsIjoieW91dGFuYWdhaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6IkRjU1JMM0VXYy1hXzQtaWdjclpMa0EiLCJuYW1lIjoieW90YSBuYWdhaSAo44KI44GG44Gh44KD44KTKSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLXd1TjZ2Q0VrNjJRL0FBQUFBQUFBQUFJL0FBQUFBQUFBTFZJL1Q2VG81WjZZMllBL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJ5b3RhIiwiZmFtaWx5X25hbWUiOiJuYWdhaSIsImxvY2FsZSI6ImphIiwiaWF0IjoxNTQ3MjgyNTk0LCJleHAiOjE1NDcyODYxOTQsImp0aSI6IjZkMTQ0ZWNiYjQ5YTlmNTgxY2QxZDgwNzFhODBiNGQ2MDhjOTA5MzQifQ.d88wuQRtkGtCs_BjiJ8X-gwHW-gXUIHAYn4cSOyEJM4HAL5ubqrXE-uI0jSqglIuNGEt_qwe-0eCEhRJvrqX59z4LjwAaXhUGg9KlB98rcFEicPyirZ7qYvrGMC-1xBESSD9qCVph1Em4r3Q9l_g_apqMXF2xkAG2_oCasgGEeLzNFTGy8Pv2J_t-WxtSg6W7uWSEiGXUjQRV5GLrpL8uwAWAdLVOMZpeHENDVho5bF5ppWns-WhKw8czFT2rHIH5nCewyg5Wy219vuf_qzr4Wh3BpGF2urRgLBEsCBdCLtZHWS2JUo2E9UKIs0jABF7rjGUT_XvR4Gz7lyRzccs5A"
+	testJWT = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhhYWQ2NmJkZWZjMWI0M2Q4ZGIyN2U2NWUyZTJlZjMwMTg3OWQzZTgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJhY2NvdW50cy5nb29nbGUuY29tIiwiYXpwIjoiNDMxMjk0OTY4MjgtcGh1OGQzOXIyZ2V0YWQ4OHEzbWZ0aWZpdTkwcjYzNG4uYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI0MzEyOTQ5NjgyOC1waHU4ZDM5cjJnZXRhZDg4cTNtZnRpZml1OTByNjM0bi5hcHBzLmdvb2dsZXVzZXJjb250ZW50LmNvbSIsInN1YiI6IjEwODg5ODkxMzIwNjgyODk4MTQxMCIsImVtYWlsIjoieW91dGFuYWdhaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6ImxtMXhuLXJodmdYRlRKTFA4RnhUT1EiLCJuYW1lIjoieW90YSBuYWdhaSAo44KI44GG44Gh44KD44KTKSIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLXd1TjZ2Q0VrNjJRL0FBQUFBQUFBQUFJL0FBQUFBQUFBTFZJL1Q2VG81WjZZMllBL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJ5b3RhIiwiZmFtaWx5X25hbWUiOiJuYWdhaSIsImxvY2FsZSI6ImphIiwiaWF0IjoxNTQ3MjkxNjUwLCJleHAiOjE1NDcyOTUyNTAsImp0aSI6IjQwNGMwNzExZjdhY2Y2NWE2NTk1YTFkZmNmNmNiZTU1MWNkYzNkYjEifQ.rDbOORUj1z7bGQrHlZjhZ6bBbcAcpz6nAQ--zQz3BX5T8DMz4nVXuwcL56suaY6LU6QnZMtwVlLewFGvBsjH4-Vm7Xs4KGg5XhTXvN0a-PQ5kCKH83FK3nojka-fAq91i6XwKlEZj_-VvS3mAMHSifPsotvKtK9Yh010m8u27d5HE5iRHUaYJIU-8blc7JoYMs8xirJGQCfmbzH5reC1_Lj-zm8K16I4ToPxTqmt6E2E6PILZL8NOyqWVemStSkBCpCQIc9EsfjBzk0HWm3XlyDFk3W9WkLfLvi8ZpxvOD3FnQXqERM-NWLcPbOqqWrv4zBa_KA7HSO_URSLYa5tUQ"
 )
 
 func TestHealthCheck(t *testing.T) {
@@ -48,4 +51,34 @@ func TestContainerGet(t *testing.T) {
 
 	json.Unmarshal(data, &containers)
 	fmt.Println(containers)
+}
+
+func TestContainerPost(t *testing.T) {
+	router := setupRouter()
+
+	image := viewmodels.ContainerImage{
+		ImageName: "nginx:latest",
+	}
+
+	data, err := json.Marshal(image)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/api/v1/containers", bytes.NewBuffer(data))
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", testJWT)
+
+	router.ServeHTTP(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(string(body))
+	assert.Equal(t, http.StatusCreated, res.StatusCode)
 }
