7b058afffe0e1a1ab3d7a76d5995fe2906663f9c
youtangai <youtanagai@gmail.com>
2019-01-12 19:46:22 +0900

    containerRepositoryのCreateContainerメソッドのリターンをerrのみした．

--- infrastructure/repositories/interfaces/iContainerRepository.go
+++ infrastructure/repositories/interfaces/iContainerRepository.go
@@ -4,6 +4,6 @@ import "github.com/fun-dev/cloud-api/domain/models"
 
 type IContainerRepository interface {
 	GetContainersByNamespace(namespace string) ([]models.Container, error)
-	CreateContainer(uniqueUserID, imageName string) (models.Container, error)
+	CreateContainer(uniqueUserID, imageName string) error
 	DeleteContainer(uniqueUserID string, containerID int64) error
 }
