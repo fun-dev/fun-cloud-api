898719ee949063942a8928a1e78a061259461fe1
youtangai <youtanagai@gmail.com>
2019-01-12 18:19:39 +0900

    containerのdomainモデルのjsonアノテーションを修正した， idをuidへ，image_idをimage_nameへ

--- domain/models/container.go
+++ domain/models/container.go
@@ -1,8 +1,8 @@
 package models
 
 type Container struct {
-	UID         string `json:id`
-	ImageName   string `json:image_id`
+	UID         string `json:uid`
+	ImageName   string `json:image_name`
 	ConnectInfo string `json:connect_info`
 	Status      string `json:status`
 }
