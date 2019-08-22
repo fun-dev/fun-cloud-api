4e3b36749f860c58b384f05ab9a821e21ce28809
RyoTozawa <ryo.for.development@gmail.com>
2019-08-22 23:41:52 +0900

    [Add] export container repository instance

--- infrastructure/module.go
+++ infrastructure/module.go
@@ -1,26 +1,14 @@
 package infrastructure
 
 import (
-	"github.com/fun-dev/cloud-api/infrastructure/repositories"
-	"github.com/fun-dev/cloud-api/infrastructure/repositories/interfaces"
-	"github.com/go-xorm/xorm"
+	"github.com/fun-dev/ccms-poc/domain/container/interfaces"
+	"github.com/fun-dev/ccms-poc/infrastructure/repository"
 )
 
 var (
-	engine        *xorm.Engine
-	UserRepo      interfaces.IUserRepository
 	ContainerRepo interfaces.IContainerRepository
 )
 
 func init() {
-	var err error
-	engine, err = repositories.NewEngine()
-	if err != nil {
-		panic(err)
-	}
-
-	// init user repo
-	UserRepo = repositories.NewUserRepository(engine)
-	// init container repo
-	ContainerRepo = repositories.NewContainerRepository()
+	ContainerRepo = repository.NewContainerRepository()
 }
