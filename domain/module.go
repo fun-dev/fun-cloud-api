de8cd9d446b88225c46f118c3eaa2ee41e2948da
RyoTozawa <ryo.for.development@gmail.com>
2019-08-22 23:41:14 +0900

    [Add] export container service instance

--- domain/module.go
+++ domain/module.go
@@ -1,22 +1,14 @@
 package domain
 
 import (
-	"github.com/fun-dev/cloud-api/domain/services"
-	"github.com/fun-dev/cloud-api/domain/services/interfaces"
+	"github.com/fun-dev/ccms-poc/domain/container"
+	"github.com/fun-dev/ccms-poc/domain/container/interfaces"
 )
 
 var (
-	UserSrv      interfaces.IUserService
 	ContainerSrv interfaces.IContainerService
 )
 
 func init() {
-	var err error
-
-	if err != nil {
-		panic(err)
-	}
-
-	UserSrv = services.NewUserService()
-	ContainerSrv = services.NewContainerService()
+	ContainerSrv = container.NewContainerService()
 }
