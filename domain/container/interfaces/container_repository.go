d876a98ef0666df6b926a494839c70cf353380aa
RyoTozawa <ryo.for.development@gmail.com>
2019-08-22 23:40:20 +0900

    [Add] container repository interface

--- domain/container/interfaces/container_repository.go
+++ domain/container/interfaces/container_repository.go
@@ -1,1 +1,8 @@
 package interfaces
+
+type IContainerRepository interface {
+	SaveDeploymentManifest(namespace, podName, yaml string) (bool, error)
+	SaveServiceManifest(namespace, podName, yaml string) (bool, error)
+	DeleteDeploymentManifest(namespace, podName string) (bool, error)
+	DeleteServiceManifest(namespace, podName string) (bool, error)
+}
