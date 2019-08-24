6347e83538bd5af08226c7bb507508c4bdc43b38
RyoTozawa <ryo.for.development@gmail.com>
2019-08-24 14:49:02 +0900

    [Add] redis client interface

--- util/client/redis.go
+++ util/client/redis.go
@@ -1,1 +1,36 @@
 package client
+
+import "github.com/go-redis/redis"
+
+type IRedis interface {
+	// Deployment
+	AddDeploymentManifest(namespace, yaml string) (bool, error)
+	DeleteDeploymentManifest(namespace string) (bool, error)
+	// Service
+	AddServiceManifest(namespace, yaml string) (bool, error)
+	DeleteServiceManifest(namespace, yaml string) (bool, error)
+}
+
+type Redis struct {
+	*redis.Client
+}
+
+func NewRedisClient(redisClient *redis.Client) IRedis {
+	return &Redis{redisClient}
+}
+
+func (r *Redis) AddDeploymentManifest(namespace, yaml string) (bool, error) {
+	return false, nil
+}
+
+func (r *Redis) DeleteDeploymentManifest(namespace string) (bool, error) {
+	return false, nil
+}
+
+func (r *Redis) AddServiceManifest(namespace, yaml string) (bool, error) {
+	return false, nil
+}
+
+func (r *Redis) DeleteServiceManifest(namespace, yaml string) (bool, error) {
+	return false, nil
+}
