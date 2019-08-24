79ab15a9a0f5d58fc4f23813eaf1b288eb1c7769
RyoTozawa <ryo.for.development@gmail.com>
2019-08-24 14:49:42 +0900

    [Add] json to model, model to json function

--- util/config/config.go
+++ util/config/config.go
@@ -1,22 +1,93 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"encoding/json"
+	"github.com/kelseyhightower/envconfig"
+	"io/ioutil"
+	v1 "k8s.io/api/apps/v1"
+	v12 "k8s.io/api/core/v1"
+	"os"
+)
 
 type externalVariables struct {
+	// Kubernetes environment
 	KubectlPath    string `envconfig:"K8S_KUBECTL_PATH"`
 	KubeConfigPath string `envconfig:"K8S_CONFIG_PATH"`
 	KubeIP         string `envconfig:"K8S_IP"`
 	ProxyAddress   string `envconfig:"PROXY_ADDRESS"`
+	// Redis environment
+	RedisAddress  string `envconfig:"REDIS_ADDRESS"`
+	RedisPassword string `envconfig:"REDIS_PASSWORD"`
+	RedisDatabase string `envconfig:"REDIS_DATABASE"`
+	//
+	APPFilePath string `envconfig:"APP_FILE_PATH"`
 }
 
 var (
-	Ext externalVariables
+	Ext            externalVariables
+	DeployTemplate v1.Deployment
+	SvcTemplate    v12.Service
+	PVTemplate     v12.PersistentVolume
+	PVCTemplate    v12.PersistentVolumeClaim
 )
-
-func init() {
-	envconfig.MustProcess("", &Ext)
-}
 
 func (ext *externalVariables) CreateK8SWebsocketProxyPath() (string, error) {
 	return "", nil
+}
+
+func init() {
+	envconfig.MustProcess("", &Ext)
+	// ---Init Deployment---
+	deploymentJSON, err := readJson(Ext.APPFilePath + "/manifest/json/pod-deployment-template.json")
+	if err != nil {
+		panic(err)
+	}
+	deploymentByte, err := bindJson(deploymentJSON)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(deploymentByte, &DeployTemplate)
+	if err != nil {
+		panic(err)
+	}
+	// ---Init Deployment---
+	// ---Init Service---
+	serviceJSON, err := readJson(Ext.APPFilePath + "/manifest/json/pod-service-template.json")
+	if err != nil {
+		panic(err)
+	}
+	serviceByte, err := bindJson(serviceJSON)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(serviceByte, &SvcTemplate)
+	if err != nil {
+		panic(err)
+	}
+	// ---Init Service---
+}
+
+func readJson(filePath string) (*os.File, error) {
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return jsonFile, nil
+}
+
+func bindJson(jsonFile *os.File) ([]byte, error) {
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	return byteValue, nil
+}
+
+func ConvertToJSON(jsonModel interface{}) (*string, error){
+	result, err := json.Marshal(jsonModel)
+	if err != nil {
+		return nil, err
+	}
+	strResult := string(result)
+	return &strResult, nil
 }
