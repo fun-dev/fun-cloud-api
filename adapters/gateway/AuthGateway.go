003b69543d05f18013a61def39fea2a13eb93c8e
RyoTozawa <ryo.for.development@gmail.com>
2019-11-19 01:32:00 +0900

    add: todo task

--- adapters/gateway/AuthGateway.go
+++ adapters/gateway/AuthGateway.go
@@ -1,1 +1,18 @@
 package gateway
+
+import (
+	"github.com/fun-dev/ccms-poc/adapters/gateway/repository"
+	"github.com/fun-dev/ccms-poc/domain/value"
+)
+
+type AuthGateway struct {}
+
+func NewAuthGateway() repository.AuthRepository {
+	return &AuthGateway{}
+}
+
+func (a AuthGateway) GetUserIDByToken(token string) (*value.UserID, error) {
+	// TODO: implement get user id from google access token
+	panic("implement me")
+}
+
