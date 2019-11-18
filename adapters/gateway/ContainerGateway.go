9d1b7ff3179cfc7184ff7e28200abc380dfb0ace
RyoTozawa <ryo.for.development@gmail.com>
2019-11-19 01:31:17 +0900

    add: two implements method

--- adapters/gateway/ContainerGateway.go
+++ adapters/gateway/ContainerGateway.go
@@ -1,50 +1,56 @@
 package gateway
 
 import (
-	"errors"
+	"context"
+	"fmt"
 	"github.com/fun-dev/ccms-poc/adapters/gateway/repository"
-	"github.com/fun-dev/ccms-poc/infrastructure/config"
+	"github.com/fun-dev/ccms-poc/domain/container"
+	"github.com/fun-dev/ccms-poc/infrastructure/apperror/repoerr"
 	"github.com/fun-dev/ccms-poc/infrastructure/driver"
 	"github.com/fun-dev/ccms-poc/infrastructure/provider"
-	"github.com/gin-gonic/gin"
 	"log"
 )
 
 // ContainerGateway is
 type ContainerGateway struct {
-	KubernetesProvider *provider.KubernetesProvider
-	RedisDriver        *driver.RedisDriver
-	Config             *config.AppVariableOnKubectl
+	provider.KubernetesProvider
+	driver.RedisDriver
 }
 
 // NewContainerGateway is
 func NewContainerGateway(
-	kubernetes *provider.KubernetesProvider,
-	redis *driver.RedisDriver,
-	config *config.AppVariableOnKubectl,
+	kubernetes provider.KubernetesProvider,
+	redis driver.RedisDriver,
 ) repository.ContainerRepository {
 	return &ContainerGateway{
 		KubernetesProvider: kubernetes,
 		RedisDriver:        redis,
-		Config:             config,
 	}
 }
 
+func (g ContainerGateway) GetAllByUserID(ctx *context.Context, id, namespace string) ([]container.Container, error) {
+	panic("implement me")
+}
+
+func (g ContainerGateway) Create(ctx *context.Context, imageName, namespace string) error {
+	panic("implement me")
+}
+
 // DeleteByContainerID is
-func (g *ContainerGateway) DeleteByContainerID(ctx *gin.Context, id string, namespace string) error {
+func (g ContainerGateway) DeleteByContainerID(ctx *context.Context, id string, namespace string) error {
 	deploymentManifest, err := g.GetDeploymentManifestByContainerID(ctx, id)
 	if err != nil {
-		return err
+		return fmt.Errorf("call ContainerGateway.GetDeploymentManifestByContainerID: %w", err)
 	}
 	log.Printf("[debug] deployment manifest (%s) on ContainerGateway.DeleteByContainerID()\n", deploymentManifest)
 	return g.KubernetesProvider.Kubectl.Execute(driver.KubectlOptionDelete, deploymentManifest, namespace)
 }
 
-func (g *ContainerGateway) GetDeploymentManifestByContainerID(ctx *gin.Context, id string) (string, error) {
+func (g ContainerGateway) GetDeploymentManifestByContainerID(ctx *context.Context, id string) (string, error) {
 	key := "deployment_" + id
 	result := g.RedisDriver.Client.Get(key)
 	if result.String() == "" {
-		return "", errors.New("not found")
+		return "", repoerr.DeploymentManifestCanNotBeFound
 	}
 	log.Printf("[debug] result (%s) on ContainerGateway.GetDeploymentManifestByContainerID()\n", result.String())
 	return result.String(), nil
