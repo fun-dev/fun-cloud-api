b1e4660ad625bdb43ccd7b6df1be6677038be455
TarakoChan <ryo.for.development@gmail.com>
2019-11-19 17:04:28 +0900

    add: authenticate middleware

---
+++ domain/Auth.go
@@ -0,0 +1,37 @@
+package domain
+
+import (
+	"github.com/fun-dev/ccms/adapters/gateway/repository"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type (
+	IAuthService interface {
+		EnsureIsExist() gin.HandlerFunc
+	}
+
+	AuthService struct {
+		repository.AuthRepository
+	}
+)
+
+func NewAuthService(aRepo repository.AuthRepository) IAuthService {
+	return &AuthService{aRepo}
+}
+
+func (a AuthService) EnsureIsExist() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		key := "Authorization"
+		accessToken := c.GetHeader(key)
+		userID, err := a.AuthRepository.GetUserIDByToken(accessToken)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"message":"your access token is not found"})
+			c.Abort()
+			return
+		}
+		key = "USER_ID"
+		c.Set(key, userID)
+		c.Next()
+	}
+}
