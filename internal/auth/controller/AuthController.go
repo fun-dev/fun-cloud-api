59cea0a559370a338343040ebd9c15d22b3fd277
Kohei Oishi <kohei.shio@gmail.com>
2019-12-09 16:15:41 +0900

    [add] AuthModel and AuthController

---
+++ internal/auth/controller/AuthController.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+type UserController struct {
+	Name           string `db:"name",json:"name"`
+	GoogleUserName string `db:"google_name",json:"google_name"`
+	AccessToken    string `db:"access_token",json:"access_token"`
+	IconURL        string `db:"icon_url",json:"icon_url"`
+	DataStore      IUserDataStore
+}
+
+type IUserController interface {
+	GET(c *gin.Context)
+	POST(c *gin.Context)
+}
+
+func NewUserController(user IUser) IUserController {
+	return &UserController{User: user}
+}
+
+func (u UserController) GET(c *gin.Context) {
+	accessToken := c.GetHeader("Authorization")
+	if accessToken == "" {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	user, err := u.User.GetByAccessToken(accessToken)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{})
+		return
+	}
+	if user != nil {
+		c.JSON(http.StatusNoContent, gin.H{})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+	return
+}
+
+func (u UserController) POST(c *gin.Context) {
+
+}
