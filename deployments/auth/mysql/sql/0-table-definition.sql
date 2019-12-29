e42d26a2b2c62d908ffbe4c6eb3abdf36eab5d03
take-2405 <take12.wa@icloud.com>
2019-12-30 00:43:06 +0900

    [fix]definition.sql

--- deployments/auth/mysql/sql/0-table-definition.sql
+++ deployments/auth/mysql/sql/0-table-definition.sql
@@ -4,8 +4,14 @@ CREATE TABLE users (
     icon_url VARCHAR(500),
     google_name VARCHAR(255),
     access_token VARCHAR(255),
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
 --     created_at DATETIME  default current_timestamp,
+ dt DATETIME_INTERVAL_CODE DEFAULT CURRENT_TIMESTAMP ,
+--  ON UPDATE CURRENT_TIMESTAMP,
+
 --     updated_at TIMESTAMP default current_timestamp on update current_timestamp,
+ ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
 --     deleted_at TIMESTAMP default current_timestamp on update current_timestamp
+DELETE FROM users WHERE timestamp < (NOW() - INTERVAL 10 MINUTE)
 );
