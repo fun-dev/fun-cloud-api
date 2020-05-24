47a4a9042cf1dcbb8c3ea24bb9994437849257f0
RyoTozawa <ryo.for.development@gmail.com>
2020-05-24 20:07:51 +0900

    update: protocol buffer

--- build/container/dev_init.sh
+++
@@ -1,4 +0,0 @@
-sleep 5
-kind create cluster
-export KUBECONFIG="$(kind get kubeconfig-path --name="kind")"
-realize start --name='container' --run
--- deployments/container/mongo/init.sh
+++
@@ -1,1 +0,0 @@
-mongo mongodb://admin:pass@container_mongo/container?authSource=admin --eval "db.getSiblingDB('container')"
